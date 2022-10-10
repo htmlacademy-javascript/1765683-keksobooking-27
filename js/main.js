eb3e33eff88bb3a9be3cf99c9b878210f32b43bd
r1zyh <kirill.ananiev2012@yandex.ru>
2022-10-10 22:37:05 +0300

    Добавит генерацию description

--- js/main.js
+++ js/main.js
@@ -15,6 +15,12 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
+const DESCRIPTIONS = [
+  'Стандартный однокомнатный номер, чаще всего с прихожей, санузлом и балконом. Номер категории Стандарт можно встретить в отелях с небольшим количеством звезд. Размер комнаты в среднем составляет 16-22 кв. м. и предназначен в основном для проживания одного или двух гостей.',
+  'Улучшенный номер, который отличается метражом. Такая категория номера в основном имеет схожие признаки со стандартным, но он выполнен в более изысканном дизайне, с применением более качественной отделки, современной техники и стильной мебели.',
+  'Компактный номер, в котором гостиная совмещена с кухней. Такой тип жилья напоминает квартиры-студии. В нем имеется всё необходимое для проживания: кровать, кухонный уголок с плитой и посудой. Площадь номера не превышает 20 кв.м., что вполне комфортно для проживания 1-2 человек.'
+];
+
 const CHECK_TIMES = ['12:00', '13:00', '14:00'];
 
 const MIN_PRICE = 1000;
@@ -64,7 +70,7 @@ const getOffer = (location = { lat: 0, lng: 0 }) => ({
   checkin: getRandomArrayElement(CHECK_TIMES),
   checkout: getRandomArrayElement(CHECK_TIMES),
   features: getRandomArrayElement(HOUSE_FEATURES),
-  description: '',
+  description: getRandomArrayElement(DESCRIPTIONS),
   photos: getRandomArrayElement(PHOTOS),
 });
 
