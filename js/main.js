5ee8a72231a7a2325e0a25ab6cce3756e78c16cd
r1zyh <kirill.ananiev2012@yandex.ru>
2022-10-11 19:41:21 +0300

    Добавит группы однотипных констант

--- js/main.js
+++ js/main.js
@@ -18,29 +18,49 @@ const PHOTOS = [
 const DESCRIPTIONS = [
   'Стандартный однокомнатный номер, чаще всего с прихожей, санузлом и балконом. Номер категории Стандарт можно встретить в отелях с небольшим количеством звезд. Размер комнаты в среднем составляет 16-22 кв. м. и предназначен в основном для проживания одного или двух гостей.',
   'Улучшенный номер, который отличается метражом. Такая категория номера в основном имеет схожие признаки со стандартным, но он выполнен в более изысканном дизайне, с применением более качественной отделки, современной техники и стильной мебели.',
-  'Компактный номер, в котором гостиная совмещена с кухней. Такой тип жилья напоминает квартиры-студии. В нем имеется всё необходимое для проживания: кровать, кухонный уголок с плитой и посудой. Площадь номера не превышает 20 кв.м., что вполне комфортно для проживания 1-2 человек.'
+  'Компактный номер, в котором гостиная совмещена с кухней. Такой тип жилья напоминает квартиры-студии. В нем имеется всё необходимое для проживания: кровать, кухонный уголок с плитой и посудой. Площадь номера не превышает 20 кв.м., что вполне комфортно для проживания 1-2 человек.',
 ];
 
 const CHECK_TIMES = ['12:00', '13:00', '14:00'];
 
-const MIN_PRICE = 1000;
-const MAX_PRICE = 2500;
-const MIN_ROOM = 1;
-const MAX_ROOM = 4;
-const MIN_GUEST = 1;
-const MAX_GUEST = 6;
-const MIN_AVATAR_ID = 1;
-const MAX_AVATAR_ID = 20;
-const MIN_LAT = 35.65;
-const MAX_LAT = 35.7;
-const MIN_LNG = 139.7;
-const MAX_LNG = 139.8;
+const PRICE = {
+  min: 1000,
+  max: 2500,
+};
+const ROOM = {
+  min: 1,
+  max: 4,
+};
+const GUEST = {
+  min: 1,
+  max: 6,
+};
+const AVATAR_ID = {
+  min: 1,
+  max: 20,
+};
+
+const LOCATION_LAT = {
+  min: 35.65,
+  max: 35.7,
+};
+
+const LOCATION_LNG = {
+  min: 139.7,
+  max: 139.8,
+};
+
 const DIGIT = 5;
 const ARRAY_LENGTH = 10;
-const AVATAR_TARGET_LENGTH = 2;
-const AVATAR_PAD_TEXT = '0';
 
-const getRandomNumber = (min = 0, max = 10, exp = 0) => {
+const AVATAR = {
+  length: 2,
+  text: '0',
+};
+
+const RANDOM_MAX_NUMBER = 10;
+
+const getRandomNumber = (min = 0, max = RANDOM_MAX_NUMBER, exp = 0) => {
   if (min < 0 || min > max) {
     return NaN;
   }
@@ -55,18 +75,18 @@ const getRandomArrayElement = (elements) =>
 
 const getAuthor = () => ({
   avatar: `img/avatars/user${getRandomNumber(
-    MIN_AVATAR_ID,
-    MAX_AVATAR_ID
-  ).padStart(AVATAR_TARGET_LENGTH, AVATAR_PAD_TEXT)}.png`,
+    AVATAR_ID.min,
+    AVATAR_ID.max
+  ).padStart(AVATAR.length, AVATAR.text)}.png`,
 });
 
 const getOffer = (location = { lat: 0, lng: 0 }) => ({
   title: 'Найдите свой приют',
   address: `${location.lat}, ${location.lng}`,
-  price: getRandomNumber(MIN_PRICE, MAX_PRICE),
+  price: getRandomNumber(PRICE.min, PRICE.max),
   type: getRandomArrayElement(HOUSE_TYPES),
-  rooms: getRandomNumber(MIN_ROOM, MAX_ROOM),
-  guests: getRandomNumber(MIN_GUEST, MAX_GUEST),
+  rooms: getRandomNumber(ROOM.min, ROOM.max),
+  guests: getRandomNumber(GUEST.min, GUEST.max),
   checkin: getRandomArrayElement(CHECK_TIMES),
   checkout: getRandomArrayElement(CHECK_TIMES),
   features: getRandomArrayElement(HOUSE_FEATURES),
@@ -75,8 +95,8 @@ const getOffer = (location = { lat: 0, lng: 0 }) => ({
 });
 
 const getLocation = () => ({
-  lat: getRandomNumber(MIN_LAT, MAX_LAT, DIGIT),
-  lng: getRandomNumber(MIN_LNG, MAX_LNG, DIGIT),
+  lat: getRandomNumber(LOCATION_LAT.min, LOCATION_LAT.max, DIGIT),
+  lng: getRandomNumber(LOCATION_LNG.min, LOCATION_LNG.max, DIGIT),
 });
 
 const getApiItem = () => {
