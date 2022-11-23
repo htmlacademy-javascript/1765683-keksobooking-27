db1d9dca0cbc46b143a411325ecf636cdab58490
Keks <keks@htmlacademy.ru>
2022-11-23 21:51:21 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/main.js
+++ 11/js/main.js
@@ -5,16 +5,33 @@ import './map.js';
 import './api.js';
 import './slider.js';
 import './filter.js';
+import './images.js';
 
 import { sendData, getData } from './api.js';
 import { disable, enable, resetForm } from './form.js';
 import { showSuccessMessage, showErrorMessage } from './messages.js';
 import { setOnFormSubmit } from './form-validation.js';
-import { setAdPins, clearMarkers, resetCoordinate, initAddress } from './map.js';
+import {
+  setAdPins,
+  clearMarkers,
+  resetCoordinate,
+  initAddress,
+} from './map.js';
 import { showAlert } from './util.js';
-import { filterOffers, onChangeFilter, resetFilters } from './filter.js';
+import {
+  filterOffers,
+  onChangeFilter,
+  resetFilters,
+  filtersContainerElement,
+  filterWatcher,
+} from './filter.js';
 
-disable();
+const onFormDisableState = () => {
+  filtersContainerElement.removeEventListener('change', filterWatcher);
+  disable();
+};
+
+onFormDisableState();
 
 const onGetDataSuccess = (offers) => {
   if (offers.length) {
